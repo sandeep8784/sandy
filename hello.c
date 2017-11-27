baaee58327509e4639749b6730566286e6696f1f
Sandeep A <sandeep.emblinux@yahoo.com>
2017-11-27 16:24:38 +0530

    Modified hello in sandy1

--- hello.c
+++ hello.c
@@ -8,6 +8,14 @@ int main()
 	printf("\n My hello world !!!\n");
 	printf("\n My hello world !!!\n");
 	printf("\n My hello world !!!\n");
+	printf("\n My hello world !!!\n");
+	printf("\n My hello world !!!\n");
+	printf("\n My hello world !!!\n");
+	printf("\n My hello world !!!\n");
+	printf("\n My hello world !!!\n");
+	printf("\n My hello world !!!\n");
+	printf("\n My hello world !!!\n");
+	printf("\n My hello world !!!\n");
 
 
 return 0;
